4a5fdaafc9c4c353ea92ab807aa22f311984bb03
Sean Hwang <14366381+KingBaba@users.noreply.github.com>
2018-05-31 13:07:28 -0400

    Added fitting

--- IVg_Analysis_TSP.m
+++ IVg_Analysis_TSP.m
@@ -1,39 +1,39 @@
 %Type out file directory
 
-Directory = 'C:\Users\Sean\Box Sync\Graduate School\Research\Data\Sensor\Source Meter\THC Sensor\2018-01-24 - Hetero-Iso-anti-THC-10 - Blank titration';
+Directory = 'C:\Users\seani\Box Sync\Graduate School\Research\Data\Sensor\Source Meter\THC Sensor\2018-05-04 - Purus Nano and Iso-sol THC Sensing in BMP-FMSI';
 cd(Directory);
 
 %Fill out information below.
 
-Material = 'Iso-sol + pyrene-COOH + THC antibody';
-Gating_Solvent = 'nanopure';
-Analyte = 'none';
-Chip_ID = 'Hetero-Iso-anti-THC-10';
-Data_Points = 101;
+Material = 'Iso-sol';
+Gating_Solvent = 'BMP-FMSI';
+Analyte = 'THC';
+Chip_ID = 'Purus Nano BMP-FMSI 1';
+Data_Points = 201;
 
 %Include any additional experimental details.
 
-Experiment_Details = 'Nanopure H2O liquid gated, 5x measurements 1 per 30 sec, 0.4 to -0.6 Vg, blank titration stability';
+Experiment_Details = '100x dilution of THC into BMP-FMSI';
 
 %List out the treatment order separated by comma.
 
 Treatment_Order = {
-    '1 100uL H2O',
-    '2 100+1x1uL H2O',
-    '3 100+2x1uL H2O',
-    '4 100+3x1uL H2O',
-    '5 100+4x1uL H2O',
-    '6 100+5x1uL H2O',
-    '7 100+6x1uL H2O',
-%    '8 100+3x1uL H2O + 2x1uL 1000 ppmv Ac',
-%    '9 100+3x1uL H2O + 3x1uL 1000 ppmv Ac',
-%    '10 100+3x1uL H2O + 3x1uL 1000 ppm Ac + 1x1uL 1000 ppmv EtOH',
-%    '11 100+3x1uL H2O + 3x1uL 1000 ppm Ac + 2x1uL 1000 ppmv EtOH',
-%    '12 100+3x1uL H2O + 3x1uL 1000 ppm Ac + 3x1uL 1000 ppmv EtOH',
-%    '13 100uL H2O',
-%    '14 100uL+1x1 uL H2O',
-%    '15 100uL+2x1 uL H2O',
-%    '16 100uL+3x1 uL H2O',
+    '1 Blank BMP-FMSI',
+    '2 Blank BMP-FMSI',
+    '3 Blank 3 Day Incubation',
+    '4 Blank Addition #1',
+    '5 Blank Addition #2',
+    '6 Blank Addition #3',
+    '7 1pg/mL THC',
+    '8 10pg/mL THC',
+    '9 100pg/mL THC',
+    '10 1ng/mL THC',
+    '11 10ng/mL THC',
+    '12 100ng/mL THC',
+    '13 10ug/mL THC',
+    '14 10ug/mL THC 15 hr incubation',
+    '15 2-wire',
+    '16 4-wire'
 %    '17 100uL+3x1 uL H2O + 1uL 0.1% THC',
 %    '18 100uL+3x1 uL H2O + 1uL 0.1% + 1uL 1% THC',
 %    '19 100uL+3x1 uL H2O + 1uL 0.1% + 1uL 1% + 1uL 10% THC',
@@ -52,19 +52,19 @@ Treatment_Order = {
 
 %Input measurement count.
 
-Measurement_Count = 5;
+Measurement_Count = 3;
 
 %Input functioning devices to analyze.
 
-Working_Devices = ['A','B','C','D'];
+Working_Devices = ['A'];
 Working_Devices_Count = length(Working_Devices);
 
 %Set your file name prefixes separated by devices.
 
-Device_A_File_Names = dir([Chip_ID, ' - Dev A - IVg*']);
-Device_B_File_Names = dir([Chip_ID, ' - Dev B - IVg*']);
-Device_C_File_Names = dir([Chip_ID, ' - Dev C - IVg*']);
-Device_D_File_Names = dir([Chip_ID, ' - Dev D - IVg*']);
+Device_A_File_Names = dir([Chip_ID, ' - IVg - Dev A - *']);
+Device_B_File_Names = dir([Chip_ID, ' - IVg - Dev B - *']);
+Device_C_File_Names = dir([Chip_ID, ' - IVg - Dev C - *']);
+Device_D_File_Names = dir([Chip_ID, ' - IVg - Dev D - *']);
 
 Combined_File_Names = struct([]);
 
